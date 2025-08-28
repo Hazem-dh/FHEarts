d3ec60e1dd486745a4de8d6474b09f7f15221c1d
hazem-dh <58445913+Hazem-dh@users.noreply.github.com>
2025-08-28 23:39:22 +0200

    added padding

--- Frontend/src/components/ConnectedLayout.tsx
+++ Frontend/src/components/ConnectedLayout.tsx
@@ -3,11 +3,10 @@ import { Navbar } from "./Navbar";
 
 export function ConnectedLayout({ children }: { children: React.ReactNode }) {
   return (
-    < div className="min-h-screen max-h-screen overflow-hidden relative">
-              <Navbar />
-      
+    <div className="min-h-screen max-h-screen overflow-hidden relative">
+      <Navbar />
       <div className="fixed inset-0 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
-        <main className="h-full flex items-center justify-center p-4 overflow-y-auto pt-16">
+        <main className="h-full flex items-center justify-center p-4 overflow-y-auto pt-32">
           <div className="w-full flex justify-center items-center min-h-full py-8">
             {children}
           </div>
